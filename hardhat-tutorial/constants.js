cd6a49aee9cf14e46a266a12b23746b3c4637ed5
oscarnicolas <oscarnicolasmozo@gmail.com>
2022-05-19 19:27:51 -0500

    DAO contract compiled

---
+++ hardhat-tutorial/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x537d06860D0C0496a0836c8449Fdf49aa659e881";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
